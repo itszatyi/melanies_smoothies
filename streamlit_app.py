7f7564acadb9a1d40717f158d2a8b61eaaafd865
itszatyi <168449460+itszatyi@users.noreply.github.com>
2024-04-30 23:34:13 +0200

    Update streamlit_app.py
    Importing requests, adding variable to call fruityvice nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -45,3 +46,7 @@ if ingredients_list:
 
 #st.write('', order_name)
 #st.write('Congrats', +order_name+ ', you have placed your order!')
+
+#New section to display fruitvice nutrition information
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
