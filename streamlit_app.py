7292d7b52c110408afd44818dc4a498ed561c0ec
itszatyi <168449460+itszatyi@users.noreply.github.com>
2024-05-03 22:11:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,6 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # Convert the snowpawk dataframe to a pandas dataframe so we can use the LOC function.
 pd_df=my_dataframe.to_pandas()
 
-
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe , max_selections=5
