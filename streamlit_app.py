90a9df2163e920912a34176d441da4a1515fe9b1
itszatyi <168449460+itszatyi@users.noreply.github.com>
2024-05-03 22:21:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,19 @@ import requests, pandas
 
 # Write directly to the app
 st.title(":cup_with_straw: Order your custom smoothie! :cup_with_straw:")
-#st.write("Pick your favourite fruits below!")
-
+st.write(
+    """Pick your favourite fruits below!
+    """
+)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 # Convert the snowpawk dataframe to a pandas dataframe so we can use the LOC function.
 pd_df=my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -35,8 +41,8 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-       
 
+    
     st.write('You have chosen ' +ingredients_string+ 'to be in your smoothie!')
     #writing ingredients list
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order,order_filled)
