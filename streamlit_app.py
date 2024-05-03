381f907e14a4164cea54fb14de251a58cb1eb47f
itszatyi <168449460+itszatyi@users.noreply.github.com>
2024-05-03 22:09:31 +0200

    Update streamlit_app.py
    Clearing up the mess

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests, pandas
+import requests, pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Order your custom smoothie! :cup_with_straw:")
@@ -13,14 +13,10 @@ st.write(
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
 # Convert the snowpawk dataframe to a pandas dataframe so we can use the LOC function.
 pd_df=my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop
 
-#fruit_present == requests.get("https://fruityvice.com/api/fruit/" + search_on)
+
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe , max_selections=5
@@ -40,13 +36,9 @@ if ingredients_list:
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
-       # if fruityvice_response:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-        #testing how result will be printed
-        print (fruityvice_response.json())
-       # else: 
-        #    st.write('This fruit is not in Fruity Vice database.')
+       
 
     st.write('You have chosen ' +ingredients_string+ 'to be in your smoothie!')
     #writing ingredients list
